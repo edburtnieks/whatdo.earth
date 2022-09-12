cb5f85aa1e375e62e7e4c51b615daf2ac7ca48c7
edburtnieks <edburtnieks@pm.me>
2022-09-12 22:34:16 +0300

    feat: connect and get country list from planetscale database

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import { Generated, Kysely } from "kysely";
+import { PlanetScaleDialect } from "../node_modules/kysely-planetscale/dist/index.mjs";
+
+interface CountryTable {
+    id: Generated<number>;
+    name: string;
+}
+
+interface Database {
+    country: CountryTable;
+}
+
+export const db = new Kysely<Database>({
+    dialect: new PlanetScaleDialect({
+        host: import.meta.env.DATABASE_HOST,
+        username: import.meta.env.DATABASE_USERNAME,
+        password: import.meta.env.DATABASE_PASSWORD,
+    }),
+});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly DATABASE_HOST: string;
+    readonly DATABASE_USERNAME: string;
+    readonly DATABASE_PASSWORD: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
