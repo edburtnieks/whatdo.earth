0895fb55e54cf3b4ba44827ea9f0ea507c05a9f8
edburtnieks <edburtnieks@pm.me>
2022-09-14 18:47:54 +0300

    try calling github workflow

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
     readonly DATABASE_HOST: string;
     readonly DATABASE_USERNAME: string;
     readonly DATABASE_PASSWORD: string;
+    readonly GITHUB_TOKEN: string;
 }
 
 interface ImportMeta {
